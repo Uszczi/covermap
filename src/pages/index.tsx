5ccdfc83395c91df2ca41bd07620313f0495f9fe
mat <mateuszpapuga24@gmail.com>
2023-02-27 22:57:06 +0100

    add leaflet map

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,79 @@
+// import Head from "next/head";
+//
+//
+// export default function Home() {
+//   return (
+//     <Layout>
+//       <Head>
+//         <title>Next.js Leaflet Starter</title>
+//         <meta
+//           name="description"
+//           content="Create mapping apps with Next.js Leaflet Starter"
+//         />
+//         <link rel="icon" href="/favicon.ico" />
+//       </Head>
+//
+//       <Section>
+//         <Container>
+//           <h1 className={styles.title}>Next.js Leaflet Starter</h1>
+//
+//           <Map
+//             className={styles.homeMap}
+//             width="800"
+//             height="400"
+//             center={DEFAULT_CENTER}
+//             zoom={12}
+//           >
+//             {({ TileLayer, Marker, Popup }) => (
+//               <>
+//                 <TileLayer
+//                   url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+//                   attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+//                 />
+//                 <Marker position={DEFAULT_CENTER}>
+//                   <Popup>
+//                     A pretty CSS3 popup. <br /> Easily customizable.
+//                   </Popup>
+//                 </Marker>
+//               </>
+//             )}
+//           </Map>
+//
+//           <p className={styles.description}>
+//             <code className={styles.code}>
+//               yarn create next-app -e
+//               https://github.com/colbyfayock/next-leaflet-starter
+//             </code>
+//           </p>
+//
+//           <p className={styles.view}>
+//             <Button href="https://github.com/colbyfayock/next-leaflet-starter">
+//               Vew on GitHub
+//             </Button>
+//           </p>
+//         </Container>
+//       </Section>
+//     </Layout>
+//   );
+// }
+//
+
 import { type NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
 import { signIn, signOut, useSession } from "next-auth/react";
 
 import { api } from "~/utils/api";
+
+import Layout from "@components/Layout";
+import Section from "@components/Section";
+import Container from "@components/Container";
+import Map from "@components/Map";
+import Button from "@components/Button";
+
+import styles from "@styles/Home.module.scss";
+
+const DEFAULT_CENTER = [38.907132, -77.036546];
 
 const Home: NextPage = () => {
   const hello = api.example.hello.useQuery({ text: "from tRPC" });
@@ -51,6 +121,47 @@ const Home: NextPage = () => {
             <AuthShowcase />
           </div>
         </div>
+
+        <Section>
+          <Container>
+            <h1 className={styles.title}>Next.js Leaflet Starter</h1>
+
+            <Map
+              className={styles.homeMap}
+              width="800"
+              height="400"
+              center={DEFAULT_CENTER}
+              zoom={12}
+            >
+              {({ TileLayer, Marker, Popup }) => (
+                <>
+                  <TileLayer
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                  />
+                  <Marker position={DEFAULT_CENTER}>
+                    <Popup>
+                      A pretty CSS3 popup. <br /> Easily customizable.
+                    </Popup>
+                  </Marker>
+                </>
+              )}
+            </Map>
+
+            <p className={styles.description}>
+              <code className={styles.code}>
+                yarn create next-app -e
+                https://github.com/colbyfayock/next-leaflet-starter
+              </code>
+            </p>
+
+            <p className={styles.view}>
+              <Button href="https://github.com/colbyfayock/next-leaflet-starter">
+                Vew on GitHub
+              </Button>
+            </p>
+          </Container>
+        </Section>
       </main>
     </>
   );
@@ -63,7 +174,7 @@ const AuthShowcase: React.FC = () => {
 
   const { data: secretMessage } = api.example.getSecretMessage.useQuery(
     undefined, // no input
-    { enabled: sessionData?.user !== undefined },
+    { enabled: sessionData?.user !== undefined }
   );
 
   return (
