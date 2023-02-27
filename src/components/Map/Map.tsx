762c06e138cfa488a1e25ec42f91a372a1461370
mat <mateuszpapuga24@gmail.com>
2023-02-27 23:14:13 +0100

    move to ts

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,16 +1,13 @@
 import dynamic from "next/dynamic";
 
-// Set default sizing to control aspect ratio which will scale responsively
-// but also help avoid layout shift
 const DynamicMap = dynamic(() => import("./DynamicMap"), {
   ssr: false,
-  suspense: true,
 });
 
 const DEFAULT_WIDTH = 600;
 const DEFAULT_HEIGHT = 600;
 
-const MMap = (props) => {
+const Map = (props) => {
   const { width = DEFAULT_WIDTH, height = DEFAULT_HEIGHT } = props;
   return (
     <div style={{ aspectRatio: width / height }}>
@@ -18,9 +15,5 @@ const MMap = (props) => {
     </div>
   );
 };
-
-const Map = dynamic(() => Promise.resolve(MMap), {
-  ssr: false,
-});
 
 export default Map;
