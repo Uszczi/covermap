98286ddb56ece877f11aff859a22fe5c2dfe7b82
mat <mateuszpapuga24@gmail.com>
2023-02-28 12:49:33 +0100

    move HEAD to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,12 @@
-export default function RootLayout({ children }) {
+import Head from "next/head";
+
+export default function RootLayout({ children }: { children: any }) {
   return (
     <html lang="en">
+      <Head>
+        <title>Cover Map</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body>{children}</body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,8 @@
 "use client";
 
 import { type NextPage } from "next";
-import Head from "next/head";
-import Link from "next/link";
-import { signIn, signOut, useSession } from "next-auth/react";
 
-import { api } from "~/utils/api";
-
-import Layout from "@components/Layout";
-import Section from "@components/Section";
-import Container from "@components/Container";
 import Map from "@components/Map";
-import Button from "@components/Button";
 
 import styles from "@styles/Home.module.scss";
 
@@ -20,10 +11,6 @@ const DEFAULT_CENTER = [38.907132, -77.036546];
 const Home: NextPage = () => {
   return (
     <>
-      <Head>
-        <title>Cover Map</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <main>
         <div>
           <h1>Cover Map</h1>
